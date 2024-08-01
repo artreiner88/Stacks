473cf698002e7f5e5d4374bb448438a8d417d31d
artreiner88 <artur.reiner88@gmail.com>
2024-08-01 20:52:15 +0200

    Initial Commit

---
+++ Stacks/StacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StacksApp.swift
+//  Stacks
+//
+//  Created by Artur Reiner on 01.08.24.
+//
+
+import SwiftUI
+
+@main
+struct StacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
